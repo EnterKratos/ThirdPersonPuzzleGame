84e1041f069e6fc20d833a7784b395068f5bd3a8
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-01-01 19:50:26 +0000

    Make player die when they fall in water

---
+++ Assets/Scripts/EnableableField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EnterKratos
+{
+    [Serializable]
+    public class EnableableField<T>
+    {
+        public bool enabled;
+        public T value;
+    }
+}
---
+++ Assets/Scripts/LockCamera.cs
@@ -0,0 +1,39 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace EnterKratos
+{
+    [ExecuteInEditMode]
+    [SaveDuringPlay]
+    [AddComponentMenu("")] // Hide in menu
+    public class LockCamera : CinemachineExtension
+    {
+        [Tooltip("Lock the camera's X position to this value")]
+        [SerializeField]
+        private EnableableField<float> xPosition;
+
+        [Tooltip("Lock the camera's Y position to this value")]
+        [SerializeField]
+        private EnableableField<float> yPosition;
+
+        [Tooltip("Lock the camera's Z position to this value")]
+        [SerializeField]
+        private EnableableField<float> zPosition;
+
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            if (stage != CinemachineCore.Stage.Body)
+            {
+                return;
+            }
+
+            var pos = state.RawPosition;
+            pos.x = xPosition.enabled ? xPosition.value : pos.x;
+            pos.y = yPosition.enabled ? yPosition.value : pos.y;
+            pos.z = zPosition.enabled ? zPosition.value : pos.z;
+            state.RawPosition = pos;
+        }
+    }
+}
