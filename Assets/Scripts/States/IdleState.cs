a74289ceb6dcb32e22f26b8905094fd63ed198d3
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-01-11 00:36:34 +0000

    Make enemies detect player when in idle state

--- Assets/Scripts/States/IdleState.cs
+++ Assets/Scripts/States/IdleState.cs
@@ -7,17 +7,44 @@ namespace EnterKratos.States
     {
         private readonly EnemyBlackboard _blackboard;
         private int _patrolPointIndex;
+        private readonly Collider[] _colliderBuffer;
+        private Coroutine _timerCoroutine;
+        private readonly EnemyStateMachine _stateMachine;
 
-        public IdleState(StateMachine<EnemyState> stateMachine, EnemyBlackboard blackboard)
+        public IdleState(EnemyStateMachine stateMachine, EnemyBlackboard blackboard)
             : base(stateMachine)
         {
+            _stateMachine = stateMachine;
             _blackboard = blackboard;
+            _colliderBuffer = new Collider[PlayerDetection.BufferSize];
         }
 
         public override void Enter()
         {
             base.Enter();
-            StateMachine.StartCoroutine(Timer());
+            _timerCoroutine = StateMachine.StartCoroutine(Timer());
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            var playerInDetectionRadius =
+                PlayerDetection.DetectPlayer(StateMachine.transform.position, _blackboard.enemy.detectionRadius,
+                    _colliderBuffer, _blackboard.playerDetectionMask);
+            if (!playerInDetectionRadius && !_blackboard.PlayerHealthSystem.Dead)
+            {
+                return;
+            }
+
+            StateMachine.StopCoroutine(_timerCoroutine);
+            StateMachine.ChangeState(EnemyState.Chase);
+        }
+
+        public override void OnDrawGizmos()
+        {
+            base.OnDrawGizmos();
+            _stateMachine.DrawDetectionRadius();
         }
 
         private IEnumerator Timer()
