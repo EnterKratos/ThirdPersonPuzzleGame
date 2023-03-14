9a1f01c0fb8f177ef1456a3b0f476514448cb324
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-03-14 22:00:15 +0000

    Add SingleSceneLight
    This prefab is enabled if there is only a single scene loaded

---
+++ Assets/Scripts/SingleSceneLight.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace EnterKratos
+{
+    [ExecuteInEditMode]
+    public class SingleSceneLight : MonoBehaviour
+    {
+        [SerializeField]
+        private new Light light;
+
+        private void OnEnable()
+        {
+            SetLightEnabledState();
+
+#if UNITY_EDITOR
+            EditorSceneManager.sceneOpened += OnSceneOpened;
+#endif
+        }
+
+        private void OnDisable()
+        {
+            SetLightEnabledState();
+
+#if UNITY_EDITOR
+            EditorSceneManager.sceneOpened -= OnSceneOpened;
+#endif
+        }
+
+        private void SetLightEnabledState()
+        {
+            light.enabled = SceneManager.sceneCount == 1;
+        }
+
+#if UNITY_EDITOR
+        private void OnSceneOpened(Scene scene, OpenSceneMode mode)
+        {
+            SetLightEnabledState();
+        }
+#endif
+    }
+}
