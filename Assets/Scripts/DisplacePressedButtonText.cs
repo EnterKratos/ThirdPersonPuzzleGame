65113218722d87c220ffa0bc6e3d81649beefc5c
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-04-02 17:30:10 +0100

    Make menu button text move on button click

---
+++ Assets/Scripts/DisplacePressedButtonText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace EnterKratos
+{
+    public class DisplacePressedButtonText : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform textRect;
+
+        [SerializeField]
+        private int offsetX;
+
+        [SerializeField]
+        private int offsetY;
+
+        private Vector3 originalPosition;
+
+        public void Down()
+        {
+            textRect.localPosition = new Vector3(
+                originalPosition.x + offsetX,
+                originalPosition.y - offsetY,
+                originalPosition.z
+            );
+        }
+
+        public void Up()
+        {
+            textRect.localPosition = originalPosition;
+        }
+
+        private void Awake()
+        {
+            originalPosition = textRect.localPosition;
+        }
+    }
+}
