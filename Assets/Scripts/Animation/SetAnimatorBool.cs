b15f5732f81a47a7ebdd5c890a09ac7c0fa3d567
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-04-02 12:06:11 +0100

    Make player start main level standing up as if they've arrived from the door

---
+++ Assets/Scripts/Animation/SetAnimatorBool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace EnterKratos.Animation
+{
+    public class SetAnimatorBool : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator animator;
+
+        [SerializeField]
+        private string propertyName;
+
+        [SerializeField]
+        private bool value;
+
+        public void SetBool()
+        {
+            animator.SetBool(propertyName, value);
+        }
+    }
+}
